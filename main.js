ee9814638d84b95da497cc7cbc8e5ebcbfcd8290
Henri Vähätalo <henri.vahatalo@tuni.fi>
2023-02-05 00:34:30 +0200

    draw 2 points on the map

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import {Feature, Map, Overlay, View} from 'ol/index.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Point} from 'ol/geom.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {useGeographic} from 'ol/proj.js';
+
+useGeographic();
+
+const place = [24.945831, 60.192059]; // [latitude, longitude]
+const place2 = [24.945831, 62.192059];
+
+const point = new Point(place);
+const point2 = new Point(place2);
 
 const map = new Map({
   target: 'map',
+
+  view: new View({
+    center: [24.748151, 64.92411],
+    zoom: 5
+  }),
   layers: [
     new TileLayer({
       source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
+    }),
+  new VectorLayer({
+    source: new VectorSource({
+      features: [new Feature(point), new Feature(point2)],
+    }),
+    style: {
+      "circle-radius": 6,
+      "circle-fill-color": "blue",
+    },
   })
+  ]
 });
+
+
